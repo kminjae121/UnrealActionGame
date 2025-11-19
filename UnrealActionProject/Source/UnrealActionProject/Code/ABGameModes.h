f7b0eaf8d30fdb7295b5e7af0f212c2b385ee524
kminjae121 <157552653+kminjae121@users.noreply.github.com>
2025-11-19 20:13:56 +0900

    feat : GameMode

---
+++ UnrealActionProject/Source/UnrealActionProject/Code/ABGameModes.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ABGameModes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALACTIONPROJECT_API AABGameModes : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
